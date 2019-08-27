96925112a85833363459cc9065f373ac8d0134d9
Aleksei Arro <aaa2@softingmsk.ru>
2019-08-27 14:15:47 +0300

    CommonJS modules applied

---
+++ source/server.js
@@ -0,0 +1,6 @@
+const User = require('./user.js');
+
+const bob = new User('Боб');
+const fedya = new User('Федя');
+
+bob.hello(fedya);
--- source/user.js
+++ source/user.js
@@ -1,4 +1,4 @@
-class User {
+module.exports = class User {
   constructor(name) {
     this.name = name;
   }
@@ -6,9 +6,4 @@ class User {
   hello(who) {
     console.log(`Привет, ${who.name}`);
   }
-}
-
-const bob = new User('Боб');
-const fedya = new User('Федя');
-
-bob.hello(fedya);
+};
