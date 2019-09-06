844bdb9756498f1b12a50eadef9b9e14a6e6ca16
Aleksei Arro <aaa2@softingmsk.ru>
2019-09-06 12:55:57 +0300

    Echo server configured

--- source/web-server/server.js
+++ source/web-server/server.js
@@ -1,23 +1,20 @@
 'use strict';
 
 const http = require('http');
+const url = require('url');
 
 const HOST = '127.0.0.1';
 const PORT = 1337;
 
-const server = http.createServer();
+const server = http.createServer((request, response) => {
+  const parsedUrl = url.parse(request.url, true);
+
+  if (parsedUrl.pathname === '/echo' && parsedUrl.query.message) {
+    response.end(parsedUrl.query.message);
+  } else {
+    response.statusCode = 404;
+    response.end('Page not found');
+  }
+});
 
 server.listen(PORT, HOST);
-
-const { emit } = server;
-server.emit = (...args) => {
-  const [evt] = args;
-  console.log(evt);
-  emit.apply(server, args);
-};
-
-let counter = 0;
-server.on('request', (request, response) => {
-  counter += 1;
-  response.end(`Hello world! Counter: ${counter}`);
-});
