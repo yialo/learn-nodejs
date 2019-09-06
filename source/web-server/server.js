ad6f960b1c0f1a981f6f5e0d5478d1c13ae027ba
Aleksei Arro <aaa2@softingmsk.ru>
2019-09-06 13:58:51 +0300

    Server request and response adjusted

--- source/web-server/server.js
+++ source/web-server/server.js
@@ -7,9 +7,13 @@ const HOST = '127.0.0.1';
 const PORT = 1337;
 
 const server = http.createServer((request, response) => {
+  console.log(request.headers);
+
   const parsedUrl = url.parse(request.url, true);
 
   if (parsedUrl.pathname === '/echo' && parsedUrl.query.message) {
+    response.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    response.statusCode = 200;
     response.end(parsedUrl.query.message);
   } else {
     response.statusCode = 404;
