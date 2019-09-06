be32381ca448dd20b0356d44d44f2c4f0cbaa88e
Aleksei Arro <aaa2@softingmsk.ru>
2019-09-06 12:43:26 +0300

    Initial web server set

--- source/web-server/server.js
+++ source/web-server/server.js
@@ -5,10 +5,19 @@ const http = require('http');
 const HOST = '127.0.0.1';
 const PORT = 1337;
 
-const server = new http.Server();
+const server = http.createServer();
 
 server.listen(PORT, HOST);
 
+const { emit } = server;
+server.emit = (...args) => {
+  const [evt] = args;
+  console.log(evt);
+  emit.apply(server, args);
+};
+
+let counter = 0;
 server.on('request', (request, response) => {
-  response.end('Hello world');
+  counter += 1;
+  response.end(`Hello world! Counter: ${counter}`);
 });
