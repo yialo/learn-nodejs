8c17ddc109eb7307a55a943b1ce9558d873b7d16
Aleksei Arro <polsck@gmail.com>
2019-09-19 00:51:02 +0300

    Loggers added

--- source/web-server/server.js
+++ source/web-server/server.js
@@ -1,27 +1,15 @@
 'use strict';
 
 const http = require('http');
-const url = require('url');
+const debug = require('debug')('server');
 
 const HOST = '127.0.0.1';
 const PORT = 1337;
 
-const server = http.createServer((request, response) => {
-  const parsedUrl = url.parse(request.url, true);
-  debugger;
+const server = http.createServer();
 
-  if (
-    request.method = 'GET'
-    && parsedUrl.pathname === '/echo'
-    && parsedUrl.query.message
-  ) {
-    response.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-    response.statusCode = 200;
-    response.end(parsedUrl.query.message);
-  } else {
-    response.statusCode = 404;
-    response.end('Page not found');
-  }
-});
+server.on('request', require('./request'));
 
 server.listen(PORT, HOST);
+
+debug('Server is running');
