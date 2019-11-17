78d6b4e525e836a309e5b8f39b2dc8d27b8c1d61
Aleksei Arro <polsck@gmail.com>
2019-11-17 23:17:55 +0300

    compare.js updated

--- source/timers/compare.js
+++ source/timers/compare.js
@@ -1,15 +1,24 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 
-fs.open(__filename, 'r', (err, file) => {
+const targetFilePath = path.resolve(__dirname, 'ref.js');
+
+fs.open(targetFilePath, 'r', (err, file) => {
+  setImmediate(() => {
+    console.log('Immediate!');
+  });
+
   console.log('I/O!');
+
+  process.nextTick(() => {
+    console.log('Next tick!');
+
+    process.nextTick(() => {
+      console.log('Another tick!');
+    });
+  });
 });
 
-setImmediate(() => {
-  console.log('Immediate!');
-});
-
-process.nextTick(() => {
-  console.log('Next tick!');
-});
+console.log('Synchronous code executes...');
