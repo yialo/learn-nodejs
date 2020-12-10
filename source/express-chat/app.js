7e3b732e6dacac2b6d07fb70004aaa1b7ce1849c
Aleksei Arro <polsck@gmail.com>
2020-12-10 21:40:42 +0300

    Add error handler middlewares to Express

--- source/express-chat/app.js
+++ source/express-chat/app.js
@@ -1,9 +1,10 @@
 'use strict';
 
-const createError = require('http-errors');
-const express = require('express');
 const path = require('path');
+
 const cookieParser = require('cookie-parser');
+const express = require('express');
+const createError = require('http-errors');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
@@ -20,8 +21,37 @@ app.use((req, res, next) => {
   }
 });
 
+app.use((req, res, next) => {
+  if (req.url === '/error') {
+    dsafasdf
+  } else {
+    next();
+  }
+});
+
+app.use((req, res, next) => {
+  if (req.url === '/break') {
+    next(new Error('Permission denied'));
+  } else {
+    next();
+  }
+});
+
 app.use((req, res) => {
   res.status(404).send('Page not found');
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.status(err.status ?? 500);
+
+  // render the error page
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // res.render('error', locals);
+
+  res.end('Internal error');
 });
 
 /*
@@ -41,17 +71,6 @@ app.use('/users', usersRouter);
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
-});
-
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
  */
 
