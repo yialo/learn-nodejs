6959d0f180c0f192433bbd00de87ffb1efa7d60a
Aleksei Arro <polsck@gmail.com>
2021-04-13 21:34:17 +0300

    Finish Express server scaffolding

--- source/express-chat/app.js
+++ source/express-chat/app.js
@@ -7,26 +7,22 @@ const express = require('express');
 const createError = require('http-errors');
 const logger = require('morgan');
 
+const { ENV } = require('./constants');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
-// error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
   res.status(err.status ?? 500);
 
-  // render the error page
-  // res.locals.message = err.message;
-  // res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // res.render('error', locals);
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === ENV.DEVELOPMENT ? err : {};
+  res.render('error', locals);
 
   res.end('Internal error');
 });
 
-/*
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -39,10 +35,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
- */
 
 module.exports = { app };
