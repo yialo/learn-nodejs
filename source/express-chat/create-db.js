63f96980bb429b8a93edd262453680db80b571ea
Aleksei Arro <polsck@gmail.com>
2021-04-24 16:27:14 +0300

    Complete lesson 35

--- source/express-chat/create-db.js
+++ source/express-chat/create-db.js
@@ -1,31 +1,14 @@
 'use strict';
 
-const { MongoClient } = require('mongodb');
+const { User } = require('./models/user');
 
-const DB_NAME = 'chat';
-const URL = 'mongodb://localhost:27017';
+const user = new User({
+  username: 'Bob',
+  password: 'secret',
+});
 
-const client = new MongoClient(URL, { useUnifiedTopology: true });
-
-client.connect(async (error) => {
-  if (error) {
-    throw error;
-  }
-
-  console.log('Connected successfully to server');
-
-  const db = client.db(DB_NAME);
-
-  const collection = db.collection('test_insert');
-
-  const docs = await collection.insertOne({ a: 1 });
-
-  const count = await collection.estimatedDocumentCount();
-  console.log('count:', count);
-
-  const cursor = collection.find({ a: 2 });
-  const results = await cursor.toArray();
-  console.log('results:', results);
-
-  client.close();
+user.save().then(() => {
+  User.findOne({ username: 'Bob' }).then((found) => {
+    console.log(found);
+  })
 });
---
+++ source/express-chat/models/user.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const crypto = require('crypto');
+
+const mongoose = require('../libs/mongoose');
+
+const schema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  hashedPassword: {
+    type: String,
+    required: true,
+  },
+  salt: {
+    type: String,
+    required: true,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+schema.methods.encryptPassword = function (password) {
+  return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+};
+
+schema.virtual('password')
+  .set(function (password) {
+    this._plainPassword = password;
+    this.salt = String(Math.random());
+    this.hashedPassword = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._plainPassword;
+  });
+
+schema.methods.checkPassword = function (password) {
+  return this.encryptPassword(password) === this.hashedPassword;
+};
+
+module.exports.User = mongoose.model('User', schema);
