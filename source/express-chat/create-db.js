2679e77a1182587f931fbfd5bac1dddc2bb2cc04
Aleksei Arro <polsck@gmail.com>
2021-05-09 18:15:53 +0300

    Decompose create-db function

--- source/express-chat/create-db.js
+++ source/express-chat/create-db.js
@@ -7,25 +7,21 @@ const open = (callback) => {
   mongoose.connection.on('open', callback);
 };
 
-const dropDatabase = async () => {};
-
-const createUsers = async () => {};
-
-const close = async () => {};
-
-open(async (openingError) => {
-  if (openingError) {
-    throw openingError;
-  }
-
+const dropDatabase = async () => {
   try {
     await mongoose.connection.dropDatabase();
     console.log('Stale database deleted');
+  } catch (dbDroppingError) {
+    throw dbDroppingError;
+  }
+};
 
-    const bob = new User({ username: 'Bob', password: '123' });
-    const bull = new User({ username: 'Bull', password: '456' });
-    const admin = new User({ username: 'admin', password: 'cool' });
+const createUsers = async () => {
+  const bob = new User({ username: 'Bob', password: '123' });
+  const bull = new User({ username: 'Bull', password: '456' });
+  const admin = new User({ username: 'admin', password: 'cool' });
 
+  return new Promise((resolve, reject) => {
     Promise.all([
       bob.save(),
       bull.save(),
@@ -33,15 +29,30 @@ open(async (openingError) => {
     ]).then(
       (results) => {
         console.log(`Saved users: ${results}`);
-        mongoose.disconnect().then(() => {
-          console.log('Disconnected');
-        });
+        resolve(results);
       },
       (savingError) => {
-        throw savingError;
+        reject(savingError);
       }
     );
-  } catch (error) {
-    throw error;
+  });
+};
+
+const close = async () => {
+  try {
+    await mongoose.disconnect();
+    console.log('Disconnected');
+  } catch (closingError) {
+    throw closingError;
   }
+};
+
+open(async (openingError) => {
+  if (openingError) {
+    throw openingError;
+  }
+
+  await dropDatabase();
+  await createUsers();
+  await close();
 });
