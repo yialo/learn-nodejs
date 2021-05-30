fd691ce2ac29181172b5665c64be0f4ec325535c
Aleksei Arro <polsck@gmail.com>
2021-05-31 01:22:04 +0300

    Fix client login code

--- source/express-chat/routes/login.js
+++ source/express-chat/routes/login.js
@@ -1,18 +1,43 @@
 'use strict';
 
 const { Router } = require('express');
+
+const { User } = require('../models/user');
+
 const loginRouter = Router();
 
 loginRouter.get('/', (_, res) => {
   res.render('login');
 });
 
-loginRouter.post('/', (req, res, next) => {
-  console.log(req.body);
+loginRouter.post('/', async (req, res, next) => {
+  console.log('--- req.params', req.params);
+  console.log('--- req.body', req.body);
+  console.log('--- req.query', req.query);
 
   const { username, password } = req.body;
 
-  res.send('READY');
+  try {
+    const user = await User.findOne({ username });
+
+    if (user) {
+      if (user.checkPassword(password)) {
+        // 200 ok
+      } else {
+        // 403 forbidden
+      }
+    } else {
+      const newUser = new User({ username, password });
+
+      try {
+        await newUser.save();
+      } catch (saveError) {
+        return next(saveError);
+      }
+    }
+  } catch (findError) {
+    next(findError);
+  }
 });
 
 module.exports.loginRouter = loginRouter;
